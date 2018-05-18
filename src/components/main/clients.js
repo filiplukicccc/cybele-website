82b159ae2b3b71037b4545550925e445cdf09dd9
cybele <kicaubuntu@gmail.com>
2018-05-18 15:39:18 +0200

    clients

--- src/components/main/clients.js
+++ src/components/main/clients.js
@@ -19,81 +19,109 @@ import { Icon } from 'semantic-ui-react'
 
 
 class Clients extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className={css.aboutUs}>
         <div>
-      <h2>O U R  &nbsp;<span> C L I E N T S</span></h2>
-      <p style={{textAlign:'center'}}>some text about cybele</p>
-      <div style={{textAlign:'center',paddingTop:'15px',display:'flex',justifyContent:'center',alignItems:'center',marginBottom:'50px'}}>
-        <span className={css.border}></span>
-        <span style={{width:'8px',height:'8px',background:'#f0d389',display:'inline-block',borderRadius:'50%',margin:'0 20px'}}></span>
-        <span className={css.border}></span>
-      </div>
-      </div>
-      <Grid  className={style.clientsDiv}>
-      <Icon name = "coffee" size='large'/>
-      <div><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
-      <Grid.Row centered style={{padding:"0px",backgroundColor:"#161e25",justifyContent:"centered"}}>
-        <Grid.Column computer={3} className={css.gridColumns}>
-        <div className={style.imgZoom}>
-          <img className={style.imgZepter} src={zepter}/>
+          <h2>O U R  &nbsp;<span> C L I E N T S</span></h2>
+          <p style={{ textAlign: 'center' }}>some text about cybele</p>
+          <div style={{ textAlign: 'center', paddingTop: '15px', display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '50px' }}>
+            <span className={css.border}></span>
+            <span style={{ width: '8px', height: '8px', background: '#f0d389', display: 'inline-block', borderRadius: '50%', margin: '0 20px' }}></span>
+            <span className={css.border}></span>
+          </div>
         </div>
-        </Grid.Column>
-        <Grid.Column computer={3} className={css.gridColumns}>
-        <div className={style.imgZoom}>
-          <img className={style.imgAerodrom} src={aerodrom}/>
-          </div>
-          </Grid.Column>
-          <Grid.Column computer={3} className={css.gridColumns}>
-          <div className={style.imgZoom}>
-           <img   src={galenika}/>
-           </div>
-           </Grid.Column>
-           {/* <div className={style.imgZoom}>
-          <img src={optiplaza}/>
-          </div> */}
-          {/* <div className={style.imgZoom}>
-          <img src={ap}/>
-          </div> */}
-          <Grid.Column computer={3} className={css.gridColumns}>
-          <div className={style.imgZoom}>
-            <img src={avanglion}/>
-          </div>
-          </Grid.Column>
-       
-          <Grid.Column computer={3} className={css.gridColumns}>
-          <div className={style.imgZoom}>
-          <img className={style.imgEli} src={eli}/>
-          </div>
-          </Grid.Column>
+        <Grid className={style.clientsDiv}>
+          <Grid.Row centered>
+            <Grid.Column computer={2}  tablet={3} mobile={14}>
+              <div className={css.iconGreyDiv}>
+                <Icon name='coffee' size='large' className={css.iconGrey} />
+                <p>DISCUSS</p>
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={2} tablet={3} mobile={7}>
+              <div className={css.iconGreyDiv}>
+                <Icon name='write' size='large' className={css.iconGrey} />
+                <p>CREATIVE CONCEPT</p>
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={2} tablet={3} mobile={7}>
+              <div className={css.iconGreyDiv}>
+                <Icon name='settings' size='large' className={css.iconGrey} />
+                <p>PRODUCTION</p>
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={2} tablet={3} mobile={7}>
+              <div className={css.iconGreyDiv}>
+                <Icon name='bug' size='large' className={css.iconGrey} />
+                <p>TESTING</p>
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={2} tablet={3} mobile={7}>
+              <div className={css.iconGreyDiv}>
+                <Icon name='smile' size='large' className={css.iconGrey} />
+                <p>HAPPY CLIENT</p>
+              </div>
+            </Grid.Column>
+            <Grid>
+              <Grid.Row centered>
+                <Grid.Column computer={10}>
+                  <div style={{ textAlign: "center", color: "#161e25",borderTop:"1px solid #161e25",borderBottom:"1px solid #161e25",marginBottom:"30px",marginTop:"30px" }}><p style={{marginTop:"30px",marginBottom:"30px"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </Grid.Row>
-          <Grid.Row centered style={{padding:"0px",backgroundColor:"#161e25",justifyContent:"centered"}}>
-          <Grid.Column computer={3} className={css.gridColumns}>
-          <div className={style.imgZoom}>
-          <img src={osce}/>
-          </div>
-          </Grid.Column>
-          <Grid.Column computer={3} className={css.gridColumns}>
-          <div className={style.imgZoom}>
-          <img src={gecko}/>
-          </div>
-          </Grid.Column>
-          {/* <div className={style.imgZoom}>
-          <img src={jelka}/>
-          </div> */}
-          <Grid.Column computer={3} className={css.gridColumns}>
-          <div className={style.imgZoom}>
-          <img src={hilton}/>
-          </div>
-          </Grid.Column>
-          <Grid.Column computer={3} className={css.gridColumns}>
-          <div className={style.imgZoom}>
-          <img src={tritacke}/>
-          </div>
-          </Grid.Column >
+          <Grid.Row centered style={{ padding: "0px", backgroundColor: "#161e25", justifyContent: "centered" }}>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img className={style.imgZepter} src={zepter} />
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img className={style.imgAerodrom} src={aerodrom} />
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img src={galenika} />
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img src={avanglion} />
+              </div>
+            </Grid.Column>
+
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img className={style.imgEli} src={eli} />
+              </div>
+            </Grid.Column>
           </Grid.Row>
-      </Grid>
+          <Grid.Row centered style={{ padding: "0px", backgroundColor: "#161e25", justifyContent: "centered" }}>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img className={style.imgOsce} src={osce} />
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img src={gecko} />
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img src={hilton} />
+              </div>
+            </Grid.Column>
+            <Grid.Column computer={3} tablet={6} mobile={12} className={css.gridColumns}>
+              <div className={style.imgZoom}>
+                <img src={tritacke} />
+              </div>
+            </Grid.Column >
+          </Grid.Row>
+        </Grid>
       </div>
     )
   }
