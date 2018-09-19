843bad9b445d6606c32a94473aeab7f5788d48a3
Filip <you@example.com>
2018-09-19 19:39:03 +0200

    dada

--- src/components/main/count.js
+++ src/components/main/count.js
@@ -11,61 +11,61 @@ import { Parallax } from 'react-parallax'
 class Count extends Component {
   render() {
     return (
-      <div style={{width:"100%"}}>
-       <div style={{background:'rgba(0,0,0,0.85)',width:"100%",height:"100%"}}>
-       <Parallax bgImage={countImg} strength={-500} className={style.bgImg}>
-         <Grid style={{margin:'0px'}}>
-          <Grid.Row className={style.countUpRow}>
-          <Grid.Column >
-          <TrackVisibility once throttleInterval={100}>
-          {
-              ({ isVisible }) => isVisible ?
-              <Grid  className={style.countDiv}>
-              {/* <div className={style.countDiv}> */}
-              <Grid.Column computer={2} mobile={8} tablet={4}>
-          <div style={{textAlign:"center"}}>
-           <CountUp className={style.countUp} start={0} end={82} duration={4.75} useEasing={true}/>
-          <p>HAPPY CLIENTS</p>
-          </div>
-          </Grid.Column >
-          <Grid.Column computer={3} mobile={8} tablet={5}>
-          <div style={{textAlign:"center"}}>
-          <CountUp className={style.countUp} start={0} end={45} duration={4.75} useEasing={true}/>
-          <p>CUPS OF COFFEE (per day)</p>
-          </div>
-          </Grid.Column>
-          <Grid.Column computer={2} mobile={8} tablet={4}>
-          <div style={{textAlign:"center"}}>
-          <CountUp className={style.countUp} start={0} end={180} duration={4.75} useEasing={true}/>
-          <p>POSITIVE FEEDBACK</p>
-          </div>
-          </Grid.Column>
-          <Grid.Column computer={2} mobile={8} tablet={4}>
-          <div style={{textAlign:"center"}}>
-          <CountUp className={style.countUp} start={0} end={63} duration={4.75} useEasing={true}/>
-          <p>FINISHED PROJECTS</p>
-          </div>
-          </Grid.Column>
-          <Grid.Column computer={2} mobile={8} tablet={4}>
-          <div style={{textAlign:"center"}}>
-          <CountUp className={style.countUp} start={0} end={8} duration={4.75} useEasing={true}/>
-          <p>OWN PROJECTS</p>
-          </div>
-          </Grid.Column>
-          <Grid.Column computer={2} mobile={8} tablet={4}>
-          <div style={{textAlign:"center"}}>
-          <CountUp className={style.countUp} start={0} end={12} duration={4.75} useEasing={true}/>
-          <p>WINNING AWARDS</p>
-          </div>
-          </Grid.Column>
-          {/* </div> */}
-          </Grid>
-          : ""}
-          </TrackVisibility>
-            </Grid.Column >
-          </Grid.Row>
-        </Grid>
-        </Parallax>
+      <div style={{ width: "100%" }}>
+        <div style={{ background: 'rgba(0,0,0,0.85)', width: "100%", height: "100%" }}>
+          <Parallax bgImage={countImg} strength={-500} className={style.bgImg}>
+            <Grid style={{ margin: '0px' }}>
+              <Grid.Row className={style.countUpRow}>
+                <Grid.Column >
+                  <TrackVisibility once throttleInterval={100}>
+                    {
+                      ({ isVisible }) => isVisible ?
+                        <Grid className={style.countDiv}>
+                          {/* <div className={style.countDiv}> */}
+                          <Grid.Column computer={2} mobile={8} tablet={4}>
+                            <div style={{ textAlign: "center" }}>
+                              <CountUp className={style.countUp} start={0} end={82} duration={4.75} useEasing={true} />
+                              <p>HAPPY CLIENTS</p>
+                            </div>
+                          </Grid.Column >
+                          <Grid.Column computer={3} mobile={8} tablet={5}>
+                            <div style={{ textAlign: "center" }}>
+                              <CountUp className={style.countUp} start={0} end={45} duration={4.75} useEasing={true} />
+                              <p>CUPS OF COFFEE (per day)</p>
+                            </div>
+                          </Grid.Column>
+                          <Grid.Column computer={2} mobile={8} tablet={4}>
+                            <div style={{ textAlign: "center" }}>
+                              <CountUp className={style.countUp} start={0} end={180} duration={4.75} useEasing={true} />
+                              <p>POSITIVE FEEDBACK</p>
+                            </div>
+                          </Grid.Column>
+                          <Grid.Column computer={2} mobile={8} tablet={4}>
+                            <div style={{ textAlign: "center" }}>
+                              <CountUp className={style.countUp} start={0} end={63} duration={4.75} useEasing={true} />
+                              <p>FINISHED PROJECTS</p>
+                            </div>
+                          </Grid.Column>
+                          <Grid.Column computer={2} mobile={8} tablet={4}>
+                            <div style={{ textAlign: "center" }}>
+                              <CountUp className={style.countUp} start={0} end={8} duration={4.75} useEasing={true} />
+                              <p>OWN PROJECTS</p>
+                            </div>
+                          </Grid.Column>
+                          <Grid.Column computer={2} mobile={8} tablet={4}>
+                            <div style={{ textAlign: "center" }}>
+                              <CountUp className={style.countUp} start={0} end={12} duration={4.75} useEasing={true} />
+                              <p>WINNING AWARDS</p>
+                            </div>
+                          </Grid.Column>
+                          {/* </div> */}
+                        </Grid>
+                        : ""}
+                  </TrackVisibility>
+                </Grid.Column >
+              </Grid.Row>
+            </Grid>
+          </Parallax>
         </div>
       </div>
     )
